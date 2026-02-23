7c0bcc23447886d446ca99ef08003ab4435d1cd0
Alisson <alisontattooskate@gmail.com>
2026-02-23 18:14:37 -0300

    fix: remove restrição local de cookies

--- lib/auth.ts
+++ lib/auth.ts
@@ -30,17 +30,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     secret: process.env.NEXTAUTH_SECRET,
     trustHost: true,
     useSecureCookies: process.env.NODE_ENV === "production",
-    cookies: {
-        sessionToken: {
-            name: process.env.NODE_ENV === "production" ? "__Secure-next-auth.session-token" : "next-auth.session-token",
-            options: {
-                httpOnly: true,
-                sameSite: "lax",
-                path: "/",
-                secure: process.env.NODE_ENV === "production",
-            },
-        },
-    },
+
     pages: { signIn: "/login" },
     callbacks: {
         async jwt({ token, user }) {
